7c0a39278c2d2ac50db769162fb703e6bfe9e59f
lcostaju <luciocj_eng@outlook.com>
2024-11-11 21:57:42 -0300

    aula 11/11

--- aula20241104/src/main/java/br/edu/iftm/tspi/pmvc/aula20241104/controller/UsuarioController.java
+++ aula20241104/src/main/java/br/edu/iftm/tspi/pmvc/aula20241104/controller/UsuarioController.java
@@ -8,15 +8,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UsuarioController {
@@ -42,4 +36,34 @@ public class UsuarioController {
 
         return "usuarioView";
     }
+
+    @GetMapping("/editar/{login}")
+    public String mostrarFormularioEdicao(@PathVariable String login, Model model) {
+        model.addAttribute("nomeUsuario", login);
+        model.addAttribute("nomes", new ArrayList<>());
+        model.addAttribute("usuarios", new ArrayList<>());
+        model.addAttribute("usuario", new Usuario());
+        return "usuarioView";
+    }
+
+    @GetMapping("/novo")
+    public String novoUsuario(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setLogin("amigao");
+        usuario.setSenha("teste");
+        usuario.setNome("Amigao ESPN");
+        usuario.setAdministrador(true);
+        model.addAttribute("usuario", usuario);
+        return "formUsuario";
+    }
+
+    @GetMapping("/vazio")
+    public String usuarioVazio(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
+        return "formUsuario";
+    }
+    
+    
+    
 }
